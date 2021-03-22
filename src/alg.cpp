85880143a85264a30ce8b963bf45fbc899a214ae
ArseniyIvanchukov <78901646+ArseniyIvanchukov@users.noreply.github.com>
2021-03-22 16:43:51 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,19 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
+        int i = 0, j = size - 1;
         int cnt = 0;
-        for (int i = 0; i < size; i++) {
-                if (arr[i] == value)
-                        cnt += 1;
+        while (i < j) {
+                int mid = (i + j) / 2;
+                if (arr[mid] < value)
+                        i = mid+1;
+                else
+                        j = mid;
+                if (arr[i]==value)
+                        while (arr[i] == value) {
+                                cnt++;
+                                i++;
+                        }
         }
         if (cnt != 0)
                 return cnt;
